53fa966467ea3220cfd37cadf4383d7c1c6e55c8
mamar0 <mattemart94@gmail.com>
2021-09-18 14:38:27 +0200

    small changes

--- web.py
+++ web.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 print(__name__)
 
-@app.route("/")
+
+@app.route("/hello")
 def hello_world():
-    return "Hello world!"
+    return "Hello!"
+
+@app.route("/hello/<name>")
+def hello_name(name):
+    return f"Hello {name}!"
+
+
+@app.route("/guest/<name>")
+def hello_guest(name):
+    return redirect(url_for("hello_name", name=name))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
